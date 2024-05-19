c754cc28402274e303328f8d307e7366fba54d56
mahfuz ahmed <mahfuzkst2@gmail.com>
2024-05-19 23:19:04 +0600

    Hollow Squre Pattern

---
+++ StarPatterns/Hollow_Squre_Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatterns
+{
+    public class Hollow_Squre_Pattern
+    {
+        public void Star()
+        {
+            Console.WriteLine("Hollow_Squre_Pattern");
+            Console.WriteLine();
+        }
+    }
+}
